d3039a77457b2c51a05c715e62e9f8cdda637330
Lia Mariel Arancibia Castillo <a20210779@pucp.edu.pe>
2024-09-25 19:37:07 -0500

    inicio

---
+++ lab5_20210779/src/main/java/com/example/lab5_20210779/Lab520210779Application.java
@@ -0,0 +1,13 @@
+package com.example.lab5_20210779;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab520210779Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab520210779Application.class, args);
+	}
+
+}
---
+++ lab5_20210779/src/test/java/com/example/lab5_20210779/Lab520210779ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab5_20210779;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab520210779ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
