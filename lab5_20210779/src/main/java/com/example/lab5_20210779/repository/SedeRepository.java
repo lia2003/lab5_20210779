eed3e0a252b2c918f18bdc18d839146d3374e377
lia2003 <a20210779@pucp.edu.pe>
2024-09-29 19:47:01 -0500

    repositorios

--- lab5_20210779/src/main/java/com/example/lab5_20210779/repository/SedeRepository.java
+++ lab5_20210779/src/main/java/com/example/lab5_20210779/repository/SedeRepository.java
@@ -1,9 +1,9 @@
 package com.example.lab5_20210779.repository;
-import com.example.lab5_20210779.entity.Profesionales;
+import com.example.lab5_20210779.entity.Sede;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProfesionalRepository extends JpaRepository<Profesionales, Integer>{
+public interface SedeRepository extends JpaRepository<Sede, Integer> {
 
 }
